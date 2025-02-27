5628e0016027060c728b1af603958846077a1b1a
RicardoGraziato <ricardograziato@gmail.com>
2025-02-26 21:57:21 -0300

    exercicio iii

--- Recursao.java
+++ Recursao.java
@@ -12,11 +12,20 @@ public class Recursao {
         return soma(a + 1, b - 1);
     }
 
+    // iii
+    public static double somaHarmonica(int n) {
+        if (n == 1){return 1.0;}
+        return 1.0 / n + somaHarmonica(n - 1);
+    }
+
     public static void main(String[] args) {
         int a = 6, b = 4;
         System.out.println(a + "x" + b + ": " + multiplicacao(a, b));
         
         int x = 3, y = 2;
         System.out.println(x + " + " + y + ": " + soma(x, y));
+
+        int n = 6;
+        System.out.println("Soma Harmônica de " + n + ": " + somaHarmonica(n));
     }
 }
